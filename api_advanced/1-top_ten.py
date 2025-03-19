dc67e017289336fb4c9050498a3131605cf77335
effiong06 <e.uyo@alustudent.com>
2025-03-19 13:55:28 +0100

    last commit c

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+A script that queries the Reddit API to fetch the top 10 hot posts from a given subreddit.
+If the subreddit does not exist, it prints OK.
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/yourusername)'}
+                
+    # Send the GET request to Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
+                            
+    # Check if the subreddit is valid (status code 404 means not found)
+    if response.status_code == 404:
+        print("OK")  # If subreddit doesn't exist, print OK
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    # If the status code is not 404, check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Get the JSON data from the response
+        posts = response.json()['data']['children']
+                                                                                        
+        # Loop through the posts and print the titles
+        for post in posts:
+            print(post['data']['title'])
+        print("OK")  # Indicate that the request was successful
+    
+    else:                 
+        # If there's another error, print OK (for any other non-404 error case)        
+        print("OK")
+
