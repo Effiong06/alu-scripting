b255289a8e045d438a432cee9e267b10ee267e6c
effiong06 <e.uyo@alustudent.com>
2025-03-20 23:14:00 +0100

    come back to this

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,25 +5,30 @@ A module that prints the titles of the top
 """
 import requests
 
+
 def top_ten(subreddit):
-    """ 
-    prints the titles of the first 10 hot posts listed in a subreddit 
     """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    A function that fetches and prints the titles
+    of the top ten hot posts from a subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(
+        url,
+        params={"after": None},
+        allow_redirects=False,
+        headers=headers,
+    )
 
     if response.status_code != 200:
-        print("OK")
+        print(None)
         return
 
-    try:
-        posts = response.json()['data']['children']
-    except KeyError:
-        print("OK")
-        return
+    jsonData = response.json()
+    data = jsonData["data"]["children"]
+    for post in data:
+        print(post.get("data", {}).get("title"))
 
-    for post in posts:
-        print(post['data']['title'])
-                              
-    print("OK")
+
+# top_ten("programming")
