37145c44df0dc1480dfc9626bba43c6f732018c8
effiong06 <e.uyo@alustudent.com>
2025-03-20 23:10:00 +0100

    pls work

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+A module that prints the titles of the top
+10 hot posts from a subreddit.
+"""
 import requests
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """ 
+    prints the titles of the first 10 hot posts listed in a subreddit 
+    """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
-                    
     response = requests.get(url, headers=headers, allow_redirects=False)
-                            
+
     if response.status_code != 200:
-        print("OK")  # Instead of None, print "OK" when invalid subreddit
+        print("OK")
         return
 
     try:
         posts = response.json()['data']['children']
     except KeyError:
-        print("OK")  # Instead of None, print "OK" if JSON structure is missing expected data
+        print("OK")
         return
 
     for post in posts:
         print(post['data']['title'])
-                                                                                                        
-    print("OK")  # Print OK after printing post titles to indicate success
-
+                              
+    print("OK")
