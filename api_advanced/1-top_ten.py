6146bb3ae86927548b70d87dadbf750ae41542de
effiong06 <e.uyo@alustudent.com>
2025-03-24 22:15:19 +0100

    finished moddoc

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,7 +3,14 @@
 import requests
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """
+    Queries the Reddit API and prints the titles of the top 10 hot posts
+    for a given subreddit.
+
+    If the subreddit is invalid, it prints None.
+    If the request is successful, it prints the titles and "OK".
+    """
+    
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
 
