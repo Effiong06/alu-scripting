ab463833255c790d53efb207d6012b0ba9ba8612
effiong06 <e.uyo@alustudent.com>
2025-03-24 22:29:37 +0100

    apadv moddoc

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,23 +2,15 @@
 """ top_ten.py """
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the top 10 hot posts
-    for a given subreddit.
-
-    If the subreddit is invalid, it prints None.
-    If the request is successful, it prints the titles and "OK".
-    """
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code != 200:
         print(None)
         return
-
     try:
         posts = response.json()['data']['children']
         for post in posts:
@@ -27,3 +19,6 @@ def top_ten(subreddit):
 
     except (KeyError, ValueError, requests.exceptions.RequestException):
         print(None)
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
